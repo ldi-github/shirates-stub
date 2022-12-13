e4c234d9668caed1c458be23d20ea0a14241acdb
ldi-github <n.senba@ldi.co.jp>
2022-12-13 21:53:42 +0900

    BuildConfig

---
+++ build/generated/source/buildConfig/main/main/shirates/shirates_stub/BuildConfig.kt
@@ -0,0 +1,13 @@
+package shirates.shirates_stub
+
+import kotlin.String
+
+object BuildConfig {
+    const val appName: String = "shirates-stub"
+
+    const val version: String = "2.0.0-SNAPSHOT"
+
+    const val packageName: String = "shirates"
+
+    const val charset: String = "UTF-8"
+}
